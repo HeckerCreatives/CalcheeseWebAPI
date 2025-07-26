927e80fd3277ee6c1850d0a5ce4f27bc8fb1673e
HeckerCreatives <creativebrainstudiosinc@gmail.com>
2025-07-26 15:37:58 +0800

    Update filters.js

--- filtertest/filters.js
+++ filtertest/filters.js
@@ -4,10 +4,10 @@ const { default: mongoose } = require("mongoose");
 exports.getcodeswithfilter = async (req, res) => {
     const {manufacturer, rewardtype, rarity, items, status} = req.query;
 
-    const pageOptions = {
-        page: parseInt(page) || 0,
-        limit: parseInt(limit) || 10,
-    };
+    // const pageOptions = {
+    //     page: parseInt(page) || 0,
+    //     limit: parseInt(limit) || 10,
+    // };
 
     const filters = {
         manufacturer: 'hbyx2',
