3fbf7c04a38d34dc93b6b01eb10043b769897d24
HeckerCreatives <creativebrainstudiosinc@gmail.com>
2025-07-26 15:37:29 +0800

    Update filters.js

--- filtertest/filters.js
+++ filtertest/filters.js
@@ -4,8 +4,13 @@ const { default: mongoose } = require("mongoose");
 exports.getcodeswithfilter = async (req, res) => {
     const {manufacturer, rewardtype, rarity, items, status} = req.query;
 
+    const pageOptions = {
+        page: parseInt(page) || 0,
+        limit: parseInt(limit) || 10,
+    };
+
     const filters = {
-        manufacturer: 'hbyx',
+        manufacturer: 'hbyx2',
         type: 'ingame',
         rarity: 'common',
         items: new mongoose.Types.ObjectId("6867da36fd0f989575d2fddf"),
@@ -15,7 +20,7 @@ exports.getcodeswithfilter = async (req, res) => {
     const result = await Code.aggregate([
     { $match: filters },
     { $group: { _id: null, total: { $sum: 1 } } }
-    ]).allowDiskUse(true); // helps with memory if needed
+    ]).allowDiskUse(true);
 
     const count = result[0]?.total || 0;
 
